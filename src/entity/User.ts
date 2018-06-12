7b2bb1898da3f7536194b81283a403d5bbb647bb
Sameer Khan <sameer_khan@student.uml.edu>
2018-06-12 09:59:11 -0400

    Added database using typeorm

---
+++ src/entity/User.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn() id: number;
+
+  @Column({ type: "text", unique: true })
+  email: string;
+
+  @Column({ type: "varchar", length: "230" })
+  firstName: string;
+
+  @Column({ type: "varchar", length: "230" })
+  lastName: string;
+
+  @Column({ type: "boolean", default: false })
+  confirmed: boolean;
+
+  @Column() age: number;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
-import {GraphQLServer} from 'graphql-yoga'
+import { GraphQLServer } from "graphql-yoga";
+import { createConnection } from "typeorm";
+import "reflect-metadata";
 
 const typeDefs = `
   type Query {
     hello(name: String): String!
   }
-`
+`;
 
 const resolvers = {
   Query: {
-    hello: (_ : any, { name }: any) => `Hello ${name || 'World'}`
-  }
-}
+    hello: (_: any, { name }: any) => `Hello ${name || "World"}`,
+  },
+};
 
-const server = new GraphQLServer({ typeDefs, resolvers })
-server.start(() => console.log('Server is running on http://localhost:4000'))
+const server = new GraphQLServer({ typeDefs, resolvers });
+createConnection().then(() => {
+  server.start(() => console.log("Server is running on http://localhost:4000"));
+});
